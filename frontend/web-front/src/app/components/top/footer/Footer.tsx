320c64f72423e3c3e6a51a01f2564c4a2e346ce5
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-02-27 06:47:58 +0900

    ディレクトリ構成変更

---
+++ frontend/web-front/src/app/components/top/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faTag, faEnvelope, faBook, faComments } from '@fortawesome/free-solid-svg-icons';
+
+export const Footer = () => {
+  return (
+      <footer className="h-24 bg-pink-50 text-white p-4">
+      </footer>
+  )
+}
--- frontend/web-front/src/app/page.tsx
+++ frontend/web-front/src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Header } from '@/app/components/header/Header'
+import { Header } from '@/app/components/top/header/Header'
+import { Hero } from './components/top/hero/Hero'
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
+        <Hero />
       </main>
     </>
   )
