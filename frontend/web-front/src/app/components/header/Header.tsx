fdb174c9af7215a229a2dea462afeb213a2fe312
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-02-27 01:25:57 +0900

    ヘッダー実装

--- frontend/web-front/src/app/components/header/Header.tsx
+++ frontend/web-front/src/app/components/header/Header.tsx
@@ -4,13 +4,38 @@ import { faHome, faTag, faEnvelope, faBook, faComments } from '@fortawesome/free
 export const Header = () => {
   return (
     <header className="h-24 bg-pink-50 text-white p-4">
-      <nav className="max-w-6xl flex justify-between items-center mx-auto h-full">
-        <ul className="flex space-x-4 text-slate-800">
-          <li><FontAwesomeIcon icon={faHome} /> HOME</li>
-          <li><FontAwesomeIcon icon={faTag} /> 料金表</li>
-          <li><FontAwesomeIcon icon={faEnvelope} /> お問い合わせ</li>
-          <li><FontAwesomeIcon icon={faBook} /> ゆるふわ日記</li>
-          <li><FontAwesomeIcon icon={faComments} /> お客様の声</li>
+      <nav className="w-full max-w-6xl mx-auto h-full">
+        <ul className="w-full px-72 flex justify-between items-center space-x-4 text-slate-800">
+          <li className="flex-col justify-center items-center">
+            <a href="#">
+              <FontAwesomeIcon icon={faHome} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
+              HOME
+            </a>
+          </li>
+          <li className="flex-col justify-center items-center">
+            <a href="#">
+              <FontAwesomeIcon icon={faTag} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
+              料金表
+            </a>
+          </li>
+          <li className="flex-col justify-center items-center">
+            <a href="#">
+              <FontAwesomeIcon icon={faEnvelope} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
+              お問い合わせ
+            </a>
+          </li>
+          <li className="flex-col justify-center items-center">
+            <a href="#">
+              <FontAwesomeIcon icon={faBook} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
+              ゆるふわ日記
+            </a>
+          </li>
+          <li className="flex-col justify-center items-center">
+            <a href="#">
+              <FontAwesomeIcon icon={faComments} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
+              お客様の声
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
