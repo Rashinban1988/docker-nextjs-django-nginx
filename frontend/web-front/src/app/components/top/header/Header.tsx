92bd72e4687aadd96337e2228bc3c5b88f6ba532
kazuya yamamoto <83887766+mest-yamaru@users.noreply.github.com>
2024-02-28 01:14:40 +0900

    Header実装

--- frontend/web-front/src/app/components/top/header/Header.tsx
+++ frontend/web-front/src/app/components/top/header/Header.tsx
@@ -1,41 +1,61 @@
+'use client'
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTag, faEnvelope, faBook, faComments } from '@fortawesome/free-solid-svg-icons';
 
-export const Header = () => {
+type HeaderProps = {
+  fixed?: boolean;
+};
+
+const navItems = [
+  { href: "#", icon: faHome, text: "HOME" },
+  { href: "#", icon: faTag, text: "料金表" },
+  { href: "#", icon: faEnvelope, text: "お問い合わせ" },
+  { href: "#", icon: faBook, text: "ゆるふわ日記" },
+  { href: "#", icon: faComments, text: "お客様の声" },
+];
+
+export const Header = ({ fixed = false }: HeaderProps) => {
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // 画面の高さを超えたかどうかをチェック
+      const shouldBeSticky = window.scrollY > window.innerHeight;
+      if (shouldBeSticky !== isSticky) {
+        setSticky(shouldBeSticky);
+      }
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
+  let headerStyle = {};
+  if (!fixed) {
+    headerStyle = {};
+  } else if (fixed && isSticky) {
+    headerStyle = { position: 'fixed', top: '0px', left: '0px' };
+  } else {
+    headerStyle = { display: 'none' };
+  }
+  const bgColor = (fixed && isSticky) ? 'bg-white' : 'bg-transparent';
+
   return (
-    <header className="h-24 bg-pink-50 text-white p-4">
+    <header style={ headerStyle } className={`w-full h-24 ${bgColor} text-white p-4`}>
       <nav className="w-full max-w-6xl mx-auto h-full">
-        <ul className="w-full px-72 flex justify-between items-center space-x-4 text-slate-800">
-          <li className="flex-col justify-center items-center">
-            <a href="#">
-              <FontAwesomeIcon icon={faHome} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
-              HOME
+        <ul className="w-full px-64 flex justify-between items-center space-x-4 text-slate-700">
+          {navItems.map((item, index) => (
+            <a href={item.href} key={index} className="hover:text-slate-500 group">
+              <li className="flex flex-col w-24 justify-center items-center">
+                <FontAwesomeIcon icon={item.icon} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} className="group-hover:animate-bounce" />
+                {item.text}
+              </li>
             </a>
-          </li>
-          <li className="flex-col justify-center items-center">
-            <a href="#">
-              <FontAwesomeIcon icon={faTag} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
-              料金表
-            </a>
-          </li>
-          <li className="flex-col justify-center items-center">
-            <a href="#">
-              <FontAwesomeIcon icon={faEnvelope} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
-              お問い合わせ
-            </a>
-          </li>
-          <li className="flex-col justify-center items-center">
-            <a href="#">
-              <FontAwesomeIcon icon={faBook} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
-              ゆるふわ日記
-            </a>
-          </li>
-          <li className="flex-col justify-center items-center">
-            <a href="#">
-              <FontAwesomeIcon icon={faComments} style={{ margin: '0 auto 10', width: '2rem', height: '2rem' }} />
-              お客様の声
-            </a>
-          </li>
+          ))}
         </ul>
       </nav>
     </header>
--- frontend/web-front/src/app/page.tsx
+++ frontend/web-front/src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/app/components/top/header/Header'
 import { Hero } from './components/top/hero/Hero'
+import { Treatment } from './components/top/treatment/Treatment'
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
+      <Header fixed={true} />
+      <main style={{ backgroundImage: "url('/bg.png')" }} className="bg-fixed min-h-screen">
         <Hero />
+        <Treatment />
       </main>
     </>
   )
